3946ac5f53159a213e3644483200e790d7e59d34
Benjaminwills <benjaminwills047@gmail.com>
2023-06-24 23:51:55 +0100

    creation of basic sidebar

--- website/src/App.js
+++ website/src/App.js
@@ -1,9 +1,11 @@
 import Footer from "./components/footer/Footer";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <div>
       <h1>HELLO WORLD!</h1>
+      <Sidebar></Sidebar>
       <Footer></Footer>
     </div>
   );
