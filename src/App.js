a233765b1249ede7b17d98c1a21fd5faf81f8b39
BenjaminWills <BenjaminWills@users.noreply.github.com>
2023-06-24 22:25:35 +0000

    deploy: 5f2e2918a16583cf0f378dad72a0e88f19b4498c

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <div>
+      <h1>HELLO WORLD!</h1>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
