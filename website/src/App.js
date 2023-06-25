4247de6ac26d05c732b76fc1ced91741ef783525
Benjaminwills <benjaminwills047@gmail.com>
2023-06-25 16:43:21 +0100

    current status

--- website/src/App.js
+++ website/src/App.js
@@ -2,7 +2,7 @@ import Footer from "./components/footer/Footer";
 import Sidebar from "./components/sidebar/Sidebar";
 import "./variables.css";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 
 // IMPORT PAGES
 import CV from './pages/CV/CV';
@@ -14,19 +14,21 @@ import Blog from './pages/blog/Blog';
 
 function App() {
   return (
-    <BrowserRouter>
-    <div className="App" id="outer-container">
-      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
-    </div>
-    <Footer></Footer>
-    <Routes>
-        <Route path="/personal_website/projects" element={<Projects />} />
-        <Route path="/personal_website/about" element={<AboutMe />} />
-        <Route path="/personal_website/cv" element={<CV />} />
-        <Route path="/personal_website/tutoring" element={<Tutoring />} />
-        <Route path="/personal_website/blog" element={<Blog />} />
-    </Routes>
-  </BrowserRouter>
+    <HashRouter>
+      <div className="App" id="outer-container">
+        <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+      </div>
+      <Footer></Footer>
+      <div>
+        <Routes>
+            <Route exact path="/personal_website/projects:optionalParam" component={<Projects />} />
+            <Route exact path="/personal_website/about:optionalParam" component={<AboutMe />} />
+            <Route exact path="/personal_website/cv:optionalParam" component={<CV />} />
+            <Route exact path="/personal_website/tutoring:optionalParam" component={<Tutoring />} />
+            <Route exact path="/personal_website/blog:optionalParam" component={<Blog />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
