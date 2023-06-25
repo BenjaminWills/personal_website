f3797e36b6bade74eb11f756562e5f0be6a2e6c3
Benjaminwills <benjaminwills047@gmail.com>
2023-06-25 12:17:01 +0100

    fixed routes, moved to app.js instead

--- website/src/App.js
+++ website/src/App.js
@@ -2,14 +2,31 @@ import Footer from "./components/footer/Footer";
 import Sidebar from "./components/sidebar/Sidebar";
 import "./variables.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// IMPORT PAGES
+import CV from './pages/CV/CV';
+import Tutoring from './pages/tutoring/Tutoring';
+import AboutMe from './pages/aboutMe/AboutMe';
+import Projects from './pages/projects/Projects';
+import Blog from './pages/blog/Blog';
+
+
 function App() {
   return (
-    <div>
-      <div className="App" id="outer-container">
+    <BrowserRouter>
+    <div className="App" id="outer-container">
       <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
     </div>
-      <Footer></Footer>
-    </div>
+    <Footer></Footer>
+    <Routes>
+        <Route path="/personal_website/projects" element={<Projects />} />
+        <Route path="/personal_website/about" element={<AboutMe />} />
+        <Route path="/personal_website/cv" element={<CV />} />
+        <Route path="/personal_website/tutoring" element={<Tutoring />} />
+        <Route path="/personal_website/blog" element={<Blog />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
--- website/src/index.js
+++ website/src/index.js
@@ -1,30 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// IMPORT PAGES
 import App from './App';
-import CV from './pages/CV/CV';
-import Tutoring from './pages/tutoring/Tutoring';
-import AboutMe from './pages/aboutMe/AboutMe';
-import Projects from './pages/projects/Projects';
-import Blog from './pages/blog/Blog';
-
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
-    <Routes>
-        <Route path="/personal_website/" element={<App />}>
-        <Route path="/personal_website/projects" element={<Projects />} />
-        <Route path="/personal_website/about" element={<AboutMe />} />
-        <Route path="/personal_website/cv" element={<CV />} />
-        <Route path="/personal_website/tutoring" element={<Tutoring />} />
-        <Route path="/personal_website/blog" element={<Blog />} />
-        </Route>
-    </Routes>
-  </BrowserRouter>
+  <App></App>
 );
