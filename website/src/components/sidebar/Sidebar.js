0f7402cd3d8c7f86037a7d9e6355c40c3d4df546
Benjaminwills <benjaminwills047@gmail.com>
2023-06-25 12:13:42 +0100

    routes added

--- website/src/components/sidebar/Sidebar.js
+++ website/src/components/sidebar/Sidebar.js
@@ -6,19 +6,19 @@ import { slide as Menu } from 'react-burger-menu';
 const Sidebar = () => {
   return (
     <Menu>
-      <a className="menu-item" href="/">
+      <a className="menu-item" href="/personal_website/">
         About me
       </a>
-      <a className="menu-item" href="/cv">
+      <a className="menu-item" href="/personal_website/cv">
         CV
       </a>
-      <a className="menu-item" href="/projects">
+      <a className="menu-item" href="/personal_website/projects">
         Project showcase
       </a>
-      <a className="menu-item" href="/blog">
+      <a className="menu-item" href="/personal_website/blog">
         Blog
       </a>
-      <a className="menu-item" href="/tutoring">
+      <a className="menu-item" href="/personal_website/tutoring">
         Tutoring
       </a>
     </Menu>
