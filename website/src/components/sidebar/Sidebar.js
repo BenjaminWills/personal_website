f221dc378d3370ab39d1a1af5fccfa516c53a8cd
Benjaminwills <benjaminwills047@gmail.com>
2023-06-25 17:05:35 +0100

    update button width

--- website/src/components/sidebar/Sidebar.js
+++ website/src/components/sidebar/Sidebar.js
@@ -6,7 +6,7 @@ import { slide as Menu } from 'react-burger-menu';
 const Sidebar = () => {
   return (
     <Menu>
-      <a className="menu-item" href="/personal_website/">
+      <a className="menu-item" href="/personal_website/about">
         About me
       </a>
       <a className="menu-item" href="/personal_website/cv">
