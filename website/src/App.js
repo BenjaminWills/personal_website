fdff585392a4d69d0812c13f4e531d145e4be021
Ben Wills <90726430+BenjaminWills@users.noreply.github.com>
2023-06-25 11:48:26 +0100

    Feature/create sidebar (#11)
    * creation of basic sidebar
    * Change website name
    * add global css vars
    * update routing

--- website/src/App.js
+++ website/src/App.js
@@ -1,9 +1,13 @@
 import Footer from "./components/footer/Footer";
+import Sidebar from "./components/sidebar/Sidebar";
+import "./variables.css";
 
 function App() {
   return (
     <div>
-      <h1>HELLO WORLD!</h1>
+      <div className="App" id="outer-container">
+      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+    </div>
       <Footer></Footer>
     </div>
   );
