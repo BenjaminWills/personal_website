8466a6b3fb4ca7b9fc5b18751de0a6cf066e56d5
Benjaminwills <benjaminwills047@gmail.com>
2023-07-09 20:16:17 +0100

    update navbar to include image

--- website/src/components/navbar/Navbar.js
+++ website/src/components/navbar/Navbar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Navbar.css';
+import logo from './IMG_1866.jpeg';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1 className="navbar-header">Ben Wills</h1>
+      <h1 className="navbar-header">
+        <img className='photo' src={logo} alt="Logo" />
+        Ben Wills
+    </h1>
     </nav>
   );
 };
