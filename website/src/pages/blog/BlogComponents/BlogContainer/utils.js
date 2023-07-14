a4e15fab9d9ac9c69f3d6bccc21b406c8f97344c
Benjaminwills <benjaminwills047@gmail.com>
2023-07-14 22:50:46 +0100

    Update the description

--- website/src/pages/blog/BlogComponents/BlogContainer/utils.js
+++ website/src/pages/blog/BlogComponents/BlogContainer/utils.js
@@ -1,7 +1,7 @@
 import './BlogContainer.css';
 
 export function splitTagsToArray(tags){
-    // Create an array of tags from the objects passed
+    // Create an array of tags from the array of objects passed
     const tagList = tags.map(
         tag => <li className = 'tag' key={tag.name}>{tag.name}</li>)
     return tagList
